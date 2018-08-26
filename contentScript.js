f6b5570678df0111c2ab2ca84e460da6e627a61a
Ishan Das Sharma <ishan.dassharma1@gmail.com>
2018-08-26 08:51:21 +0530

    Collapse button now appears even when you click links (thanks AJAX)

--- contentScript.js
+++ contentScript.js
@@ -1,28 +1,38 @@
-console.log("Active on this page!");
+function runScript() {
+  // Inject the button into every comment's header
+  $(".details").append("<button class='collapser'>Collapse</button>");
 
-// Inject the button into every comment's header
-$(".details").append("<button class='collapser'>Collapse</button>");
+  // Also, re-add whenever a link is clicked
+  // this is because dev.to uses AJAX loading and I can't figure out a simple solution
+  // to the problem
+  $("a").click(function(e) {
+    runScript();
+  });
 
-$(".collapser").click(function(e) {
-  // First, collapse the parent comment's body and actions, leaving the header
-  $(this)
-    .parent()
-    .parent()
-    .children(":nth-child(2), :nth-child(3)")
-    .slideToggle();
+  $(".collapser").click(function(e) {
+    // First, collapse the parent comment's body and actions, leaving the header
+    $(this)
+      .parent()
+      .parent()
+      .children(":nth-child(2), :nth-child(3)")
+      .slideToggle();
 
-  // Next, collapse all children
-  $(this)
-    .parent()
-    .parent()
-    .parent()
-    .children(":not(:first-child)")
-    .slideToggle();
+    // Next, collapse all children
+    $(this)
+      .parent()
+      .parent()
+      .parent()
+      .children(":not(:first-child)")
+      .slideToggle();
 
-  // Finally, change the text on the button to reflect the state
-  if ($(this).text() == "Collapse") {
-    $(this).text("Expand");
-  } else {
-    $(this).text("Collapse");
-  }
-});
+    // Finally, change the text on the button to reflect the state
+    if ($(this).text() == "Collapse") {
+      $(this).text("Expand");
+    } else {
+      $(this).text("Collapse");
+    }
+  });
+}
+
+// initial run to get it going
+runScript();
